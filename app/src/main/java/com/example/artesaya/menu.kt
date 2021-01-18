737993daa2adc7f4fbfba9377680d9421f1c1216
Adriel-lab-desing <77516788+Adriel-lab-desing@users.noreply.github.com>
2021-01-18 09:29:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/artesaya/menu.kt
@@ -0,0 +1,11 @@
+package com.example.artesaya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
